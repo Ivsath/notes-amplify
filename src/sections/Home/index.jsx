1c3eb44d503baf21c91b76cff70b9a4df56cd7b1
Irvin Reyes <reyes.yerp@gmail.com>
2020-11-02 11:34:25 -0600

    Display list of notes

--- src/sections/Home/index.jsx
+++ src/sections/Home/index.jsx
@@ -1,7 +1,66 @@
+import { useEffect, useReducer } from 'react'
+import { API } from 'aws-amplify'
+import { List } from 'antd'
+
+import { listNotes } from '../../graphql/queries'
+
+const initialState = {
+  notes: [],
+  loading: true,
+  error: false,
+  form: { name: '', description: '' },
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'SET_NOTES':
+      return { ...state, notes: action.notes, loading: false }
+    case 'ERROR':
+      return { ...state, loading: false, error: true }
+    default:
+      return state
+  }
+}
+
 export const Home = () => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(() => {
+    fetchNotes()
+  }, [])
+
+  const fetchNotes = async () => {
+    try {
+      const notesData = await API.graphql({ query: listNotes })
+      dispatch({ type: 'SET_NOTES', notes: notesData.data.listNotes.items })
+    } catch (err) {
+      console.log('error: ', err)
+      dispatch({ type: 'ERROR' })
+    }
+  }
+
+  const renderItem = (item) => {
+    return (
+      <List.Item style={styles.item}>
+        <List.Item.Meta title={item.name} description={item.description} />
+      </List.Item>
+    )
+  }
+
   return (
-    <div>
-      <p>Home Component</p>
+    <div style={styles.container}>
+      <List
+        loading={state.loading}
+        dataSource={state.notes}
+        renderItem={renderItem}
+      />
     </div>
   )
+}
+
+const styles = {
+  container: { padding: 20 },
+  input: { marginBottom: 10 },
+  item: { textAlign: 'left' },
+  p: { color: '#1890ff' },
 }
